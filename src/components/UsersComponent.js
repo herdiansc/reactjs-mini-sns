0aeb7a4c343d166430ff117c3d9f368ee43e75a7
herdiansc <herdiansc@gmail.com>
2020-03-20 21:40:58 +0700

    remove add user action

--- src/components/UsersComponent.js
+++ src/components/UsersComponent.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from "react-redux";
-import { getUsers, createUser } from '../actions';
-import NotificationComponent from './NotificationComponent.js'
+import { getUsers } from '../actions';
 
 const mapStateToProps = state => {
     return { users: state.users };
@@ -10,8 +9,7 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        getUsers: () => dispatch(getUsers()),
-        createUser: payload => dispatch(createUser(payload))
+        getUsers: () => dispatch(getUsers())
     };
 }
 
@@ -21,17 +19,6 @@ class Users extends React.Component {
         this.state = {
             isFormShown: false
         };
-        this.name = React.createRef();
-        this.username = React.createRef();
-        this.email = React.createRef();
-        this.phone = React.createRef();
-        this.city = React.createRef();
-        this.suite = React.createRef();
-        this.zipcode = React.createRef();
-        this.website = React.createRef();
-        this.street = React.createRef();
-        this.lat = React.createRef();
-        this.long = React.createRef();
     }
     componentDidMount() {
         this.props.getUsers();
@@ -48,105 +35,6 @@ class Users extends React.Component {
         )
     }
 
-    handleShowUserForm(isShown, e) {
-        this.setState({isFormShown: isShown})
-    }
-
-    formCreateUser() {
-        if (this.state.isFormShown) {
-            return (
-                <div className="row">
-                    <div className="col-md-12">
-                        <form className="border mb-4 mt-4 p-4">
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Name" ref={this.name} />
-                                </div>
-                            </div>
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Username" ref={this.username} />
-                                </div>
-                            </div>
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Email" ref={this.email} />
-                                </div>
-                            </div>
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Website" ref={this.website} />
-                                </div>
-                            </div>
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Phone" ref={this.phone} />
-                                </div>
-                            </div>
-                            <hr />
-                            <div className="form-row mb-4 mt-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Street" ref={this.street} />
-                                </div>
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Suite" ref={this.suite} />
-                                </div>
-                            </div>
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Lat" ref={this.lat} />
-                                </div>
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Long" ref={this.long} />
-                                </div>
-                            </div>
-                            <div className="form-row mb-4">
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="City" ref={this.city} />
-                                </div>
-                                <div className="col">
-                                    <input type="text" className="form-control" placeholder="Zipcode" ref={this.zipcode} />
-                                </div>
-                            </div>
-                            <div className="form-row">
-                                <div className="col">
-                                    <div className="btn-group float-right" role="group">
-                                        <button type="button" className="btn bg-default border"  onClick={(e) => this.handleShowUserForm(false, e)}>Cancel</button>
-                                        <button type="button" className="btn text-white bg-kumparan" onClick={(e) => this.handleSubmitUserForm(e)}>Submit</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            )
-        }
-    }
-
-    handleSubmitUserForm(e) {
-        this.props.createUser({
-            name: this.name.current.value,
-            username: this.username.current.value,
-            email: this.email.current.value,
-            address: {
-                street: this.street.current.value,
-                suite: this.suite.current.value,
-                city: this.city.current.value,
-                zipcode: this.zipcode.current.value,
-                geo: {
-                    lat: "-38.2386",
-                    lng: "57.2232"
-                }
-            },
-            phone: this.phone.current.value,
-            website: this.website.current.value,
-        })
-        .then(() => {
-            this.props.getUsers();
-        });
-        this.handleShowUserForm(false);
-    }
-
     render() {
         return (
             <div className="container">
@@ -154,16 +42,7 @@ class Users extends React.Component {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="albums mt-4 mb-4 pt-4 pb-4">
-                            <div className="row">
-                                <div className="col-md-6"><h3 className="">Users</h3></div>
-                                <div className="col-md-6">
-                                    <button type="button" className="btn btn-sm text-white bg-kumparan float-right" onClick={(e) => { this.handleShowUserForm(true, e)}}>Add</button>
-                                </div>
-                            </div>
-                            
-                            <NotificationComponent />
-                            { this.formCreateUser() }
-
+                            <h3 className="">Users</h3>
                             <div className="row">
                                 <div className="col-md-12">
                                     <div className="list-group">
