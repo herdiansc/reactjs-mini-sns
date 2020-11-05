9c7824f02e7c1d3cb319aa55ed8aada6f5ae8898
herdian <herdian@cashbac.com>
2020-11-05 17:42:52 +0700

    Add build folder for githubio

---
+++ build/precache-manifest.c79fdd5cbd7e0babd60ae13fac929dba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5189a712541efee966db0773f0516035",
+    "url": "/reactjs-mini-sns/build/index.html"
+  },
+  {
+    "revision": "3ebe27d090272106e4f0",
+    "url": "/reactjs-mini-sns/build/static/js/2.c075bcb6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/reactjs-mini-sns/build/static/js/2.c075bcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2a4d87e0fec51df313",
+    "url": "/reactjs-mini-sns/build/static/js/main.5883758c.chunk.js"
+  },
+  {
+    "revision": "8fc8f9485faf4eb14732",
+    "url": "/reactjs-mini-sns/build/static/js/runtime-main.e3ddf587.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.c386878cbe387572deba7a9d1b0f389c.js"
+  "/reactjs-mini-sns/build/precache-manifest.c79fdd5cbd7e0babd60ae13fac929dba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reactjs-mini-sns/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
